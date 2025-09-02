0ab75f22bfd75a9ff76109776e593b9c457d21ac
Abiodn Olutoye <abiodunolutoye1912@gmail.com>
2025-09-02 14:59:18 +0100

    i  implement dynamic task list and count in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-function App(props) {
+import { useState } from "react";
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-3", name: "Code", completed: false },
+  { id: "todo-4", name: "Read", completed: false },
+  { id: "todo-5", name: "Exercise", completed: false }
+];
+
+function App() {
+  const [tasks, setTasks] = useState(DATA);
+
   return (
     <div className="todoapp stack-large">
       <h1>Taskdo</h1>
+
+      {/* Add Task Form */}
       <form>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
@@ -19,10 +34,12 @@ function App(props) {
           Add
         </button>
       </form>
+
+      {/* Filter Buttons */}
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
-          <span>all</span>
+          <span>All</span>
           <span className="visually-hidden"> tasks</span>
         </button>
         <button type="button" className="btn toggle-btn" aria-pressed="false">
@@ -36,59 +53,36 @@ function App(props) {
           <span className="visually-hidden"> tasks</span>
         </button>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+
+      {/* Dynamic Task Count */}
+      <h2 id="list-heading">
+        {tasks.filter((task) => !task.completed).length} tasks remaining
+      </h2>
+
+      {/* Task List */}
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked />
-            <label className="todo-label" htmlFor="todo-0">
-              Eat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li>
+        aria-labelledby="list-heading"
+      >
+        {tasks.map((task) => (
+          <li className="todo stack-small" key={task.id}>
+            <div className="c-cb">
+              <input id={task.id} type="checkbox" defaultChecked={task.completed} />
+              <label className="todo-label" htmlFor={task.id}>
+                {task.name}
+              </label>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn">
+                Edit <span className="visually-hidden">{task.name}</span>
+              </button>
+              <button type="button" className="btn btn__danger">
+                Delete <span className="visually-hidden">{task.name}</span>
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
